5b9213b99dcb34e3e1c62036fface15a7c60ce9a
Minjie Xu <minjie.xu@gmail.com>
2020-08-27 13:54:17 -0500

    add a test for google cloud storage: need to set GOOGLE_APPLICATION_CREDENTIALS before running

---
+++ server/src/test/java/com/uudaddy/SpringBootReact/demo/googleCloudStorageTest.java
@@ -0,0 +1,30 @@
+package com.uudaddy.SpringBootReact.demo;
+
+import org.junit.Test;
+import com.google.api.gax.paging.Page;
+import com.google.auth.oauth2.ComputeEngineCredentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.google.common.collect.Lists;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+// https://cloud.google.com/docs/authentication/getting-started#auth-cloud-implicit-java
+
+public class googleCloudStorageTest {
+    @Test
+    public void authImplicit() {
+        // If you don't specify credentials when constructing the client, the client library will
+        // look for credentials via the environment variable GOOGLE_APPLICATION_CREDENTIALS.
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+
+        System.out.println("Buckets:");
+        Page<Bucket> buckets = storage.list();
+        for (Bucket bucket : buckets.iterateAll()) {
+            System.out.println(bucket.toString());
+        }
+    }
+
+}
