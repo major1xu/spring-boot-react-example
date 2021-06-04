9c89ee221358438b52a61fd674f8548e650f2bfd
major1xu <minjie.xu@gmail.com>
2021-06-03 19:10:28 -0500

    more DI

--- server/src/main/java/com/uudaddy/SpringBootReact/demo/beer/ClassMainSetterDI.java
+++ server/src/main/java/com/uudaddy/SpringBootReact/demo/beer/ClassMainSetterDI.java
@@ -3,10 +3,20 @@ package com.uudaddy.SpringBootReact.demo.beer;
 public class ClassMainSetterDI {
     public static void main(String... args) {
         ClassASetterDI classA = new ClassASetterDI();
+        /*
         ClassB classB = new ClassB();
-
         classA.setClassB(classB);
+        */
+        ImprovedClassB improvedClassB = new ImprovedClassB();
+        classA.setClassB(improvedClassB);
+
 
         System.out.println("Ten Percent: " + classA.tenPercent());
+
+        ClassASetterDI classA2 = new ClassASetterDI();
+/* We are hiding the ClassB dependency in ClassA because by reading the constructor signature, we cannot identify
+its dependencies right away. The code below causes a NullPointerException on runtime:
+ */
+        System.out.println("Ten Percent: " + classA2.tenPercent()); // NullPointerException here
     }
 }
---
+++ server/src/main/java/com/uudaddy/SpringBootReact/demo/beer/ImprovedClassB.java
@@ -0,0 +1,6 @@
+package com.uudaddy.SpringBootReact.demo.beer;
+
+// https://medium.com/groupon-eng/dependency-injection-in-java-9e9438aa55ae
+class ImprovedClassB extends ClassB {
+    // content omitted
+}
